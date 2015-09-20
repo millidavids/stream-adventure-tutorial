d983cb3f3b56be386beac2f3590cc81a4ad1d02c
David Yurek <david.a.yurek@gmail.com>
2015-09-20 19:05:54 -0400

    http server

--- program.js
+++ program.js
@@ -1,5 +1,18 @@
-var concat = require('concat-stream');
+var http = require('http'),
+    through = require('through'),
+    port = process.argv[2],
+    server = http.createServer(function(req, res) {
+        if (req.method === 'POST') {
+            req.pipe(through(write, end)).pipe(res);
+        }
+    });
 
-process.stdin.pipe(concat(function(buf) {
-    process.stdout.write(buf.toString().split('').reverse().join(''));
-}));
+function write(buf) {
+    this.queue(buf.toString().toUpperCase());
+}
+
+function end() {
+    this.queue(null);
+}
+
+server.listen(port);
