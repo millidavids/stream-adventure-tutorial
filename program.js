f802b5008abf8337c4a8ed086d8f90503c9ecb0d
David Yurek <david.a.yurek@gmail.com>
2015-09-20 19:11:04 -0400

    http client

--- program.js
+++ program.js
@@ -1,18 +1,2 @@
-var http = require('http'),
-    through = require('through'),
-    port = process.argv[2],
-    server = http.createServer(function(req, res) {
-        if (req.method === 'POST') {
-            req.pipe(through(write, end)).pipe(res);
-        }
-    });
-
-function write(buf) {
-    this.queue(buf.toString().toUpperCase());
-}
-
-function end() {
-    this.queue(null);
-}
-
-server.listen(port);
+var request = require('request');
+process.stdin.pipe(request.post('http://localhost:8099')).pipe(process.stdout);
