3dd21fb1387b4587123bbb3077d843c6c4f65da2
David Yurek <david.a.yurek@gmail.com>
2015-09-20 15:40:03 -0400

    concat

--- program.js
+++ program.js
@@ -1,14 +1,5 @@
-var through = require('through'),
-    split = require('split'),
-    nlines = 1;
+var concat = require('concat-stream');
 
-function write(buf) {
-    this.queue(
-        buf.toString()[
-            nlines++ % 2 === 0? 'toUpperCase' : 'toLowerCase']() + '\n');
-}
-
-process.stdin
-    .pipe(split())
-    .pipe(through(write))
-    .pipe(process.stdout);
+process.stdin.pipe(concat(function(buf) {
+    process.stdout.write(buf.toString().split('').reverse().join(''));
+}));
