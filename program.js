f2657f85629eca81d186f5c065b51b6e5495ca67
David Yurek <david.a.yurek@gmail.com>
2015-09-20 20:14:05 -0400

    crypt

--- program.js
+++ program.js
@@ -1,40 +1,5 @@
-var combine = require('stream-combiner'),
-    split = require('split')
-    through = require('through')
-    zlib = require('zlib');
+var crypto = require('crypto'),
+    passphrase = process.argv[2],
+    stream = crypto.createDecipher('AES256', passphrase);
 
-module.exports = function() {
-    var genreInfo;
-
-    function write(buf) {
-        if (buf.length === 0) {
-            return;
-        }
-
-        buf = JSON.parse(buf);
-        if (buf.type === 'genre') {
-            if(genreInfo) {
-                this.queue(JSON.stringify(genreInfo) + '\n');
-            }
-
-            genreInfo = {
-                name: buf.name,
-                books: []
-            }
-
-        } else {
-            genreInfo.books.push(buf.name);
-        }
-    };
-
-    function end() {
-        this.queue(JSON.stringify(genreInfo) +'\n');
-        this.queue(null);
-    };
-
-    return combine(
-        split(),
-        through(write, end),
-        zlib.createGzip()
-    );
-};
+process.stdin.pipe(stream).pipe(process.stdout);
